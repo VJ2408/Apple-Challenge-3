50994fe954cd7bfd40236a580b479e2ad883fc8f
VJ2408 <vijai.adithya24@gmail.com>
2024-09-02 09:51:54 +0800

    Initial Commit

---
+++ Apple Challenge 3/Apple_Challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Challenge_3App.swift
+//  Apple Challenge 3
+//
+//  Created by Vijai Adithya on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Challenge 3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Challenge 3
+//
+//  Created by Vijai Adithya on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
