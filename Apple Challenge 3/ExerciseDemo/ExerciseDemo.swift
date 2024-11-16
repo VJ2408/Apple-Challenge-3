7690b585c7ac7ffcd09f0026267b247f67260b97
VJ2408 <vijai.adithya24@gmail.com>
2024-11-16 16:19:16 +0800

    Images

---
+++ Apple Challenge 3/ExerciseDemo/ExerciseDemo.swift
@@ -0,0 +1,18 @@
+//
+//  Activity.swift
+//  Apple Challenge 3
+//
+//  Created by Dhanush  on 14/11/24.
+//
+
+import SwiftUI
+
+struct Activity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Activity()
+}
--- Apple Challenge 3/ExerciseDemo/ExerciseDemoVideo.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ExerciseDemoVideo.swift
-//  Apple Challenge 3
-//
-//  Created by Vijai Adithya on 14/11/24.
-//
-
