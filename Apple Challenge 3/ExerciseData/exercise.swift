4f4fbde6645ca1fcd9d79e6af6e664deb73697e1
VJ2408 <vijai.adithya24@gmail.com>
2024-11-16 16:48:06 +0800

    Saviour

--- Apple Challenge 3/ExerciseData/exercise.swift
+++ Apple Challenge 3/ExerciseData/exercise.swift
@@ -57,21 +57,21 @@ let morningHard = [
 ]
 
 let nightEz = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Yoga", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Running", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Walking", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName")
+    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "5 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.run", name: "Walking", intensity: "Easy", time: "10 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.walk", name: "Stretches", intensity: "Easy", time: "3 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 let nightMid = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Yoga", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Running", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Walking", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"),videoName: "placeholderName")
+    Exercise(symbolName: "figure.mind.and.body", name: "Bridge", intensity: "Medium", time: "2 mins", instructions: "1. Lie on your back with knees bent and feet flat on the ground, hip-width apart. \n 2. Place arms by your sides, palms facing down.\n 3. Push through your heels to lift your hips toward the ceiling, forming a straight line from shoulders to knees. \n4. Squeeze your glutes at the top, then lower slowly.", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.run", name: "Jogging", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.walk", name: "Skipping", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 let nightHard = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Running", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
-    Exercise(symbolName: "figure.mind.and.body", name: "Walking", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName")
+    Exercise(symbolName: "figure.mind.and.body", name: "Lunges", intensity: "Hard", time: "3 min", instructions: "1. Lie on your back with knees bent and feet flat on the floor. \n 2. Place your hands behind your head or cross them over your chest.\n 3. Engage your core and lift your upper body toward your knees. \n 4. Lower yourself back down with control. \n 5. Repeat for the desired number of reps.", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Sit-ups", intensity: "Hard", time: "3 min", instructions: "1. Lie on your back with knees bent and feet flat on the floor. \n 2. Place your hands behind your head or cross them over your chest.\n 3. Engage your core and lift your upper body toward your knees. \n 4. Lower yourself back down with control. \n 5. Repeat for the desired number of reps.", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Plank", intensity: "Hard", time: "2 mins", instructions: "1. Lie face down with forearms on the ground, elbows directly under shoulders, and toes tucked under. \n 2. Push up into a straight line from head to heels, engaging your core.\n 3. Keep your back flat, neck neutral, and avoid letting your hips sag or rise.\n 4. Hold the position for as long as possible while maintaining good form", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 
