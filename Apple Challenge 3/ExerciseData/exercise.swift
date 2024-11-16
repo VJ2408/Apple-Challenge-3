e061301fb3b2987114e19bd7f372dbff9cf93cce
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2024-11-16 16:11:18 +0800

    edited exercises

--- Apple Challenge 3/ExerciseData/exercise.swift
+++ Apple Challenge 3/ExerciseData/exercise.swift
@@ -39,21 +39,21 @@ struct Exercise: Identifiable {
 
 //data for morning, afternoon, night. categorised by difficulty
 let morningEz = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "5 mins", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.run", name: "Walking", intensity: "Easy", time: "10 mins", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.walk", name: "Stretches", intensity: "Easy", time: "3 mins", instructions: "Yololol", image: Image("placeholderImage")),
+    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "5 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.run", name: "Walking", intensity: "Easy", time: "10 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.walk", name: "Stretches", intensity: "Easy", time: "3 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 let morningMid = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Bridge", intensity: "Medium", time: "2 mins", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.run", name: "Jogging", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.walk", name: "Skipping", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage")),
+    Exercise(symbolName: "figure.mind.and.body", name: "Bridge", intensity: "Medium", time: "2 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.run", name: "Jogging", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.walk", name: "Skipping", intensity: "Medium", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 let morningHard = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Lunges", intensity: "Hard", time: "3 min", instructions: "Move your body into a deep lunge by taking a big step forward with your right leg while keeping the left leg outstretched towards the back. Keep your feet flat against the ground as you progress deeper into the pose. Place both hands on the ground next to your right foot. Release your right arm from the ground and turn your torso to face your leg. Keep your right arm extended up towards the ceiling. Return your right arm to the starting position and open up again. Repeat 8 times on each side. This morning exercise is a great quad stretch.", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.mind.and.body", name: "Sit-ups", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.mind.and.body", name: "Plank", intensity: "Hard", time: "2 mins", instructions: "Yololol", image: Image("placeholderImage")),
+    Exercise(symbolName: "figure.mind.and.body", name: "Lunges", intensity: "Hard", time: "3 min", instructions: "Move your body into a deep lunge by taking a big step forward with your right leg while keeping the left leg outstretched towards the back. Keep your feet flat against the ground as you progress deeper into the pose. Place both hands on the ground next to your right foot. Release your right arm from the ground and turn your torso to face your leg. Keep your right arm extended up towards the ceiling. Return your right arm to the starting position and open up again. Repeat 8 times on each side. This morning exercise is a great quad stretch.", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Sit-ups", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Plank", intensity: "Hard", time: "2 mins", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
 ]
 
 let nightEz = [
@@ -69,9 +69,9 @@ let nightMid = [
 ]
 
 let nightHard = [
-    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.mind.and.body", name: "Running", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage")),
-    Exercise(symbolName: "figure.mind.and.body", name: "Walking", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"))
+    Exercise(symbolName: "figure.mind.and.body", name: "Meditating", intensity: "Easy", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Running", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName"),
+    Exercise(symbolName: "figure.mind.and.body", name: "Walking", intensity: "Hard", time: "3 min", instructions: "Yololol", image: Image("placeholderImage"), videoName: "placeholderName")
 ]
 
 
